deea3113c6621ba50aa4570abed8ef2dc90690c1
Bridger Davidson <mvbdavidson@gmail.com>
2025-09-09 14:59:03 -0700

    Models complete

--- VehicleClassLibrary2.2/Models/MotorcycleModel.cs
+++ VehicleClassLibrary2.2/Models/MotorcycleModel.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Bridger Davidson
+ * CST-250
+ * 09/09/2025
+ * VehicleClassLibrary - Car Model
+ * Activity 1 - In class activity
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +13,50 @@ using System.Threading.Tasks;
 
 namespace VehicleClassLibrary2._2.Models
 {
-    internal class MotorcycleModel
+
+    internal class MotorcycleModel : VehicleModel
     {
+        // Class level props
+        public bool HasSideCar { get; set; }
+        public decimal SeatHeight { get; set; }
+
+        /// <summary>
+        /// Default Constructor with VehicleModel as base
+        /// </summary>
+        public MotorcycleModel() : base()
+        {
+            HasSideCar = false;
+            SeatHeight = 0;
+        }
+
+        /// <summary>
+        /// Perameterized Constructor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="make"></param>
+        /// <param name="model"></param>
+        /// <param name="year"></param>
+        /// <param name="price"></param>
+        /// <param name="numOfWheels"></param>
+        /// <param name="hasSideCar"></param>
+        /// <param name="seatHeight"></param>
+        public MotorcycleModel(int id, string make, string model, int year, decimal price, int numOfWheels, bool hasSideCar, decimal seatHeight) : base(id, make, model, year, price, numOfWheels)
+        {
+            HasSideCar = hasSideCar;
+            SeatHeight = seatHeight;
+        }
+
+        /// <summary>
+        /// ToString Method for printing a motorcycle
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            // Print the vehicle in the following format using string interpolation...
+            // 1: 2001 Harley Davidson Cruiser with 2 wheels and a 38 inch tall seat with(out) a side car - $1,500.00
+            string sideCar = HasSideCar ? "with" : "without";
+            return $"{Id}: {Year} {Make} {Model} with {NumOfWheels} and a {SeatHeight} inch tall seat {sideCar} a side car - {Price:C2}";
+        }
+
     }
 }
--- VehicleClassLibrary2.2/Models/PickupModel.cs
+++ VehicleClassLibrary2.2/Models/PickupModel.cs
@@ -7,14 +7,54 @@
  */
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VehicleClassLibrary2._2.Models
 {
-    internal class PickupModel
+    internal class PickupModel : VehicleModel
     {
+        // Class level props
+        public decimal BedLength { get; set; }
+        public bool HasTopper { get; set; }
 
+        /// <summary>
+        /// Default Constructor using VehicleModel as base
+        /// </summary>
+        public PickupModel() : base()
+        {
+            BedLength = 0;
+            HasTopper = false;
+        }
+
+        /// <summary>
+        /// Perameterized Constructor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="make"></param>
+        /// <param name="model"></param>
+        /// <param name="year"></param>
+        /// <param name="price"></param>
+        /// <param name="numOfWheels"></param>
+        /// <param name="hasTopper"></param>
+        /// <param name="bedLength"></param>
+        public PickupModel(int id, string make, string model, int year, decimal price, int numOfWheels, bool hasTopper, decimal bedLength) : base(id, make, model, year, price, numOfWheels)
+        {
+            HasTopper = hasTopper;
+            BedLength = bedLength;
+        }
+
+        /// <summary>
+        /// ToString Method for printing a pickup
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            // 1: 1994 Chevy Siverado with 4 wheels and a 7 foot bed with(out) a topper - $1,500.00
+            string topper = HasTopper ? "with" : "without";
+            return $"{Id}: {Year} {Make} {Model} with {NumOfWheels} wheels and a {BedLength} foot bed {topper} a topper - {Price:C2}";
+        } 
     }
 }
